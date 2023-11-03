a6f1ea8c379eb615a4cee0afaea6c8296ec7c376
mdxshakil <mdxshakil1767@gmail.com>
2023-11-03 20:04:24 +0600

    Added theme toggler on navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
+"use client";
 import Link from "next/link";
 import Container from "./ui/container";
 import { Button } from "./ui/button";
 import { ShoppingCart, Sun, Moon } from "lucide-react";
 import ProfileButton from "./ui/ProfileButton";
+import { useTheme } from "next-themes";
 
 export default function Header() {
+  const { theme, setTheme } = useTheme();
+
   const routes = [
     {
       href: "/",
@@ -55,6 +59,7 @@ export default function Header() {
               size={"icon"}
               aria-label="Toggle Theme"
               className="mr-6"
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
             >
               <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
               <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
